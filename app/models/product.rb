9923f81cc524b7036db3b3367677521e74276d25
Said Maadan <saidmaadan@users.noreply.github.com>
2015-06-25 10:30:28 -0500

    Install and setup devise for seller authentication

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,7 +1,14 @@
 class Product < ActiveRecord::Base
+	if Rails.env.development?
+		has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "no-images.jpg"
+	else
 	has_attached_file :image, :styles => { :medium => "200x200>", :thumb => "100x100>" }, :default_url => "no-images.jpg",
 					   				:storage => :dropbox,
-								    :dropbox_credentials => Rails.root.join("config/dropbox.yml")
+								    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
 								    # :dropbox_options => {...}
-   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
+								     :path => ":style/:id_:filename"
+	end
+	 validates :name, :description, :price, presence: true
+	 validates :price, numericality: { greater_than: 0 }
+ 	 validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 end
